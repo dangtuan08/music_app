4fc22437fb422d025c3f0dd07ecf48915feac656
Tuan <tuanou17@gmail.com>
2022-05-13 14:17:08 +0700

    sửa link songs

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ const app = {
         {
             name: "Đi Trong Mùa Hè",
             singer: "Đen x Trần Tiến",
-            path: "https://data.chiasenhac.com/down2/2244/4/2243850-34687502/128/Di%20Trong%20Mua%20He%20-%20Den_%20Tran%20Tien.mp3",
+            path: "https://data.chiasenhac.com/down2/2244/5/2243850-34687502/128/Di%20Trong%20Mua%20He%20-%20Den_%20Tran%20Tien.mp3",
             image: "https://data.chiasenhac.com/data/cover/163/162329.jpg"
         },
         {
             name: "Bài Này Chill Phết",
             singer: "Đen; Min",
-            path: "https://data.chiasenhac.com/down2/2179/4/2178590-1df95ef6/128/Bai%20Nay%20Chill%20Phet%20-%20Den_%20Min.mp3",
+            path: "https://data.chiasenhac.com/down2/2179/5/2178590-1df95ef6/128/Bai%20Nay%20Chill%20Phet%20-%20Den_%20Min.mp3",
             image:
                 "https://data.chiasenhac.com/data/cover/143/142394.jpg"
         },
@@ -45,20 +45,20 @@ const app = {
             name: "Mang Tiền Về Cho Mẹ",
             singer: "Đen; Nguyên Thảo",
             path:
-                "https://data.chiasenhac.com/down2/2215/4/2214701-52396a51/128/Mang%20Tien%20Ve%20Cho%20Me%20-%20Den_%20Nguyen%20Thao.mp3",
+                "https://data.chiasenhac.com/down2/2215/5/2214701-52396a51/128/Mang%20Tien%20Ve%20Cho%20Me%20-%20Den_%20Nguyen%20Thao.mp3",
             image: "https://data.chiasenhac.com/data/cover/153/152045.jpg"
         },
         {
             name: "Lối Nhỏ",
             singer: "Đen; Phương Anh Đào",
-            path: "https://data.chiasenhac.com/down2/2211/4/2210420-cad860c9/128/Loi%20Nho%20-%20Den_%20Phuong%20Anh%20Dao.mp3",
+            path: "https://data.chiasenhac.com/down2/2211/5/2210420-cad860c9/128/Loi%20Nho%20-%20Den_%20Phuong%20Anh%20Dao.mp3",
             image:
                 "https://data.chiasenhac.com/data/cover/152/151033.jpg"
         },
         {
             name: "Đi Về Nhà",
             singer: "Đen; JustaTee",
-            path: "https://data.chiasenhac.com/down2/2179/4/2178291-6e126457/128/Di%20Ve%20Nha%20-%20Den_%20JustaTee.mp3",
+            path: "https://data.chiasenhac.com/down2/2179/5/2178291-6e126457/128/Di%20Ve%20Nha%20-%20Den_%20JustaTee.mp3",
             image:
                 "https://data.chiasenhac.com/data/cover/133/132896.jpg"
         },
@@ -66,7 +66,7 @@ const app = {
             name: "Cảm Ơn",
             singer: "Đen; Biên",
             path:
-                "https://data.chiasenhac.com/down2/2211/4/2210422-3e31b147/128/Cam%20On%20-%20Den_%20Bien.mp3",
+                "https://data.chiasenhac.com/down2/2211/5/2210422-3e31b147/128/Cam%20On%20-%20Den_%20Bien.mp3",
             image:
                 "https://data.chiasenhac.com/data/cover/152/151035.jpg"
         },
