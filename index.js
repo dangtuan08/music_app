41d3d7e4c7a77d11aee9ef6ec464807f954252ad
Tuan <tuanou17@gmail.com>
2022-05-14 17:10:31 +0700

    next song

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ const app = {
         {
             name: "Đi Trong Mùa Hè",
             singer: "Đen x Trần Tiến",
-            path: "https://data.chiasenhac.com/down2/2244/5/2243850-34687502/128/Di%20Trong%20Mua%20He%20-%20Den_%20Tran%20Tien.mp3",
+            path: "https://data.chiasenhac.com/down2/2244/6/2243850-34687502/128/Di%20Trong%20Mua%20He%20-%20Den_%20Tran%20Tien.mp3",
             image: "https://data.chiasenhac.com/data/cover/163/162329.jpg"
         },
         {
             name: "Bài Này Chill Phết",
             singer: "Đen; Min",
-            path: "https://data.chiasenhac.com/down2/2179/5/2178590-1df95ef6/128/Bai%20Nay%20Chill%20Phet%20-%20Den_%20Min.mp3",
+            path: "https://data.chiasenhac.com/down2/2179/6/2178590-1df95ef6/128/Bai%20Nay%20Chill%20Phet%20-%20Den_%20Min.mp3",
             image:
                 "https://data.chiasenhac.com/data/cover/143/142394.jpg"
         },
@@ -45,20 +45,20 @@ const app = {
             name: "Mang Tiền Về Cho Mẹ",
             singer: "Đen; Nguyên Thảo",
             path:
-                "https://data.chiasenhac.com/down2/2215/5/2214701-52396a51/128/Mang%20Tien%20Ve%20Cho%20Me%20-%20Den_%20Nguyen%20Thao.mp3",
+                "https://data.chiasenhac.com/down2/2215/6/2214701-52396a51/128/Mang%20Tien%20Ve%20Cho%20Me%20-%20Den_%20Nguyen%20Thao.mp3",
             image: "https://data.chiasenhac.com/data/cover/153/152045.jpg"
         },
         {
             name: "Lối Nhỏ",
             singer: "Đen; Phương Anh Đào",
-            path: "https://data.chiasenhac.com/down2/2211/5/2210420-cad860c9/128/Loi%20Nho%20-%20Den_%20Phuong%20Anh%20Dao.mp3",
+            path: "https://data.chiasenhac.com/down2/2211/6/2210420-cad860c9/128/Loi%20Nho%20-%20Den_%20Phuong%20Anh%20Dao.mp3",
             image:
                 "https://data.chiasenhac.com/data/cover/152/151033.jpg"
         },
         {
             name: "Đi Về Nhà",
             singer: "Đen; JustaTee",
-            path: "https://data.chiasenhac.com/down2/2179/5/2178291-6e126457/128/Di%20Ve%20Nha%20-%20Den_%20JustaTee.mp3",
+            path: "https://data.chiasenhac.com/down2/2179/6/2178291-6e126457/128/Di%20Ve%20Nha%20-%20Den_%20JustaTee.mp3",
             image:
                 "https://data.chiasenhac.com/data/cover/133/132896.jpg"
         },
@@ -66,7 +66,7 @@ const app = {
             name: "Cảm Ơn",
             singer: "Đen; Biên",
             path:
-                "https://data.chiasenhac.com/down2/2211/5/2210422-3e31b147/128/Cam%20On%20-%20Den_%20Bien.mp3",
+                "https://data.chiasenhac.com/down2/2211/6/2210422-3e31b147/128/Cam%20On%20-%20Den_%20Bien.mp3",
             image:
                 "https://data.chiasenhac.com/data/cover/152/151035.jpg"
         },
@@ -108,6 +108,14 @@ const app = {
         const _this = this;
         const cdWidth = cd.offsetWidth
 
+        // Xử lý animation cho cdThumb
+        const animateThumb = cdThumb.animate({ transform: 'rotate(0deg)', transform: 'rotate(360deg)' }, {
+            duration: 8000,
+            iterations: Infinity
+        })
+        animateThumb.pause()
+
+
         // Xử lý scroll để phóng to thu nhỏ hình thumb
         document.onscroll = function () {
             let scroll = window.scrollY || document.documentElement.scrollTop;
@@ -133,21 +141,90 @@ const app = {
         audio.onplay = function () {
             _this.isPlaying = true;
             player.classList.add('playing')
+            animateThumb.play()
         }
 
         // Xử lý nếu thực sự đang pause thì add class
         audio.onpause = function () {
             player.classList.remove('playing')
             _this.isPlaying = false
+            animateThumb.pause()
+        }
+
+        audio.ontimeupdate = function () {
+            // console.log();
+            progress.value = Math.floor(audio.currentTime * 100 / audio.duration)
+            // progress.value = audio.currentTime * 100 / audio.duration
+        }
+
+        progress.oninput = function () {
+            let progressTime = progress.value * audio.duration / 100;
+            audio.currentTime = Math.floor(progressTime);
+        }
+
+        progress.addEventListener('mousedown', () => {
+            audio.pause()
+            animateThumb.pause()
+        })
+
+        progress.addEventListener('mouseup', () => {
+            audio.play()
+            animateThumb.play()
+        })
+
+        nextBtn.onclick = function () {
+            _this.nextSong()
+            animateThumb.cancel()
+            audio.play()
+        }
+
+        prevBtn.onclick = function () {
+            _this.prevSong()
+            animateThumb.cancel()
+            audio.play()
+        }
+
+        // Xử lý nút random
+        randomBtn.onclick = function () {
+            console.log(_this.isRandom);
+            if(_this.isRandom){
+                _this.isRandom= false
+                randomBtn.classList.remove('active')
+            } else {
+                console.log('1');
+                _this.isRandom= true
+                randomBtn.classList.add('active')
+                
+            }
         }
 
     },
+
     loadCurrentSong: function () {
         heading.textContent = this.currentSong.name;
         cdThumb.style.backgroundImage = `url(${this.currentSong.image})`
         audio.src = this.currentSong.path
 
     },
+
+    nextSong: function () {
+        // console.log(this.songs.length);
+        this.currentIndex++;
+        if (this.currentIndex >= this.songs.length)
+            this.currentIndex = 0
+
+        this.loadCurrentSong()
+    },
+
+    prevSong: function () {
+        // console.log(this.songs.length);
+        this.currentIndex--;
+        if (this.currentIndex < 0 )
+            this.currentIndex = this.songs.length
+
+        this.loadCurrentSong()
+    },
+
     start: function () {
         // Định nghĩa các thuộc tính cho obj
         this.defineProperties()
